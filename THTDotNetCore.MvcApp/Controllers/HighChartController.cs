48bf3dce993b3ec29464755fdfc7017c04093618
thargyi <thanthtettin@gmail.com>
2024-02-13 16:25:52 +0630

    [Complete PieWithLegend HighJs]

---
+++ THTDotNetCore.MvcApp/Controllers/HighChartController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using THTDotNetCore.MvcApp.Models;
+
+namespace THTDotNetCore.MvcApp.Controllers
+{
+    public class HighChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PieWithLegend()
+        {
+            var model = new HighChartModel()
+            {
+                name = "Brands",
+                datas = new List<Data>()
+                {
+                    new Data
+                    {
+                        name = "Chrome",
+                        y = 74.77,
+                        sliced = true,
+                        selected = true,
+                    },
+                    new Data
+                    {
+                        name = "Edge",
+                        y = 12.82,
+                       
+                    },
+                     new Data
+                    {
+                        name = "Firefox",
+                        y = 4.63,
+
+                    }, new Data
+                    {
+                        name = "Safari",
+                        y = 2.44,
+
+                    },
+                      new Data
+                    {
+                        name = "Internet Explorer",
+                        y = 2.02,
+
+                    },
+                       new Data
+                    {
+                        name = "Other",
+                        y = 3.28,
+
+                    },
+
+
+                }
+               
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ THTDotNetCore.MvcApp/Models/HighChartModel.cs
@@ -0,0 +1,22 @@
+﻿namespace THTDotNetCore.MvcApp.Models
+{
+    public class HighChartModel
+    {
+        public string name { get; set; }
+
+        public List<Data> datas { get; set; }
+
+    }
+
+    public class Data
+    {
+        public string name { get; set; }
+        public double y { get; set; }
+
+        public Boolean? sliced { get; set; }
+
+        public Boolean? selected { get; set; }
+
+    }
+
+}
